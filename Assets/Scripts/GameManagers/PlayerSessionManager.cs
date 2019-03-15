d74bdcef7d95e5c69a68ba42c75982b825f4802c
Ian Thomson <ianthomson92@hotmail.co.uk>
2019-03-15 16:59:39 +0000

    Added PlayerManager and Screenshot taker

---
+++ Assets/Scripts/GameManagers/PlayerSessionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class PlayerSessionManager : MonoBehaviour
+{
+    [HideInInspector]
+    public Guid PlayerSession;
+    [HideInInspector]
+    public string ScreenshotFolderPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerSession = Guid.NewGuid();
+        ScreenshotFolderPath = $"Screenshots/{PlayerSession.ToString()}/";
+
+        Directory.CreateDirectory(ScreenshotFolderPath);
+    }
+}
