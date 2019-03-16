b70e315f1e33ff6eaf8e72d82904ad7fee005339
Atuuh <not-another-one@hotmail.com>
2019-03-16 16:33:55 +0000

    Updated shader moved some scene stuff about

---
+++ Assets/Scripts/TriggerSystem/MoveTo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    public float AnimateTime = 1f;
+    public Transform Target;
+
+    private bool movementStarted = false;
+    private Vector3 velocity;
+
+    void Update()
+    {
+        if (movementStarted)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref velocity, AnimateTime);
+
+            if (transform.position == Target.position)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void StartMovement()
+    {
+        movementStarted = true;
+    }
+}
